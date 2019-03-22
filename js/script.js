7eb25005fe4fbacb9935a762b8afbd9bcc3908e5
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-03-22 21:17:27 +0100

    Add js for chart

--- js/script.js
+++ js/script.js
@@ -168,3 +168,33 @@ function closePopup(popup) {
     popup.style.display = 'none';
   };
 };
+
+
+// CHART 
+var ctx = document.getElementById('general-chart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09"],
+    datasets: [{
+      label: "Signups",
+      backgroundColor: '#8DBEC8',
+      borderColor: '#8DBEC8',
+      data: [350, 205, 220, 380, 420, 400, 310, 280, 300],
+    },
+    {
+      label: "FTD",
+      backgroundColor: '#F29E4E',
+      borderColor: '#F29E4E',
+      data: [400, 190, 300, 260, 440, 90, 210, 501, 320],
+    },
+    {
+      label: "Earned",
+      backgroundColor: '#71B374',
+      borderColor: '#71B374',
+      data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+      hidden: true,
+    }]
+  },
+});
